bc0dfcfe0c0f7c55ba89a43207183f3a0ad8a895
yourok <8yourok8@mail.ru>
2020-01-29 21:15:45 +0300

    fix m3u

--- src/server/web/helpers/M3u.go
+++ src/server/web/helpers/M3u.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"fmt"
 	"net/url"
+	"path/filepath"
 
 	"server/settings"
 	"server/torr"
@@ -13,7 +14,7 @@ func MakeM3ULists(torrents []*settings.Torrent, host string) string {
 	m3u := "#EXTM3U\n"
 
 	for _, t := range torrents {
-		m3u += "#EXTINF:0 type=\"playlist\", " + t.Name + "\n"
+		m3u += "#EXTINF:-1 type=\"playlist\", " + t.Name + "\n"
 		m3u += host + "/torrent/play?link=" + url.QueryEscape(t.Magnet) + "&m3u=true&fname=" + utils.CleanFName(t.Name+".m3u") + "\n\n"
 	}
 	return m3u
@@ -24,7 +25,11 @@ func MakeM3UPlayList(tor torr.TorrentStats, magnet string, host string) string {
 
 	for _, f := range tor.FileStats {
 		if GetMimeType(f.Path) != "*/*" {
-			m3u += "#EXTINF:-1, " + f.Path + "\n"
+			fn := filepath.Base(f.Path)
+			if fn == "" {
+				fn = f.Path
+			}
+			m3u += "#EXTINF:-1, " + fn + "\n"
 			mag := url.QueryEscape(magnet)
 			m3u += host + "/torrent/play?link=" + mag + "&file=" + fmt.Sprint(f.Id) + "\n\n"
 		}
