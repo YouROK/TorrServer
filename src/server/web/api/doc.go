332d0e7ba457917fb312d1bb9c77d2459f0f19c9
YouROK <8yourok8@mail.ru>
2020-11-06 17:01:24 +0300

    migrate

---
+++ src/server/web/api/doc.go
@@ -0,0 +1,34 @@
+package api
+
+/*
+API
+
+echo version
+
+{
+	add torrent
+	remove torrent
+	list all torrents
+	get torrent
+	stream torrent
+} all in one function
+
+{
+	get viewed files mark in torrent
+	set viewed files mark in torrent
+	rem viewed files mark in torrent
+} all in one
+
+{
+	get m3u list of one torrent
+	get m3u list of one torrent and resume play
+	get m3u list of all torrent
+}
+
+{
+	get settings
+	set settings
+}
+
+???
+*/
