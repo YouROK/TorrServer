9cf6b6f03e08df53fcfcf2701b551b96ddd3271b
YouROK <8yourok8@mail.ru>
2020-12-04 16:55:20 +0300

    update main page from react

--- src/server/web/pages/pages.go
+++ src/server/web/pages/pages.go
@@ -2,16 +2,9 @@ package pages
 
 import (
 	"github.com/gin-gonic/gin"
+	"server/web/pages/template"
 )
 
 func mainPage(c *gin.Context) {
-	c.HTML(200, "mainPage", nil)
-}
-
-func apijsPage(c *gin.Context) {
-	c.HTML(200, "apijsPage", nil)
-}
-
-func mainjsPage(c *gin.Context) {
-	c.HTML(200, "mainjsPage", nil)
+	c.Data(200, "text/html; charset=utf-8", []byte(template.MainPage))
 }
--- src/server/web/pages/route.go
+++ src/server/web/pages/route.go
@@ -1,17 +1,9 @@
 package pages
 
 import (
-	"server/web/pages/template"
-
 	"github.com/gin-gonic/gin"
 )
 
-var temp *template.Template
-
 func SetupRoute(route *gin.Engine) {
-	temp = template.InitTemplate(route)
-
 	route.GET("/", mainPage)
-	// route.GET("/api.js", apijsPage)
-	// route.GET("/main.js", mainjsPage)
 }
