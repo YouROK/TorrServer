442c1fc27af627a4ec07d65e094054b076c3447c
YouROK <8yourok8@mail.ru>
2020-12-08 17:08:33 +0300

    update

--- src/server/torr/stream.go
+++ src/server/torr/stream.go
@@ -24,7 +24,8 @@ func (t *Torrent) Stream(fileIndex int, req *http.Request, resp http.ResponseWri
 
 	sets.SetViewed(&sets.Viewed{t.Hash().HexString(), fileIndex})
 
-	resp.Header().Set("Connection", "close")
+	//TODO проверить почему плеер постоянно переподключается
+	resp.Header().Set("Connection", "keep-alive")
 	resp.Header().Set("ETag", httptoo.EncodeQuotedString(fmt.Sprintf("%s/%s", t.Hash().HexString(), file.Path())))
 
 	http.ServeContent(resp, req, file.Path(), time.Time{}, reader)
