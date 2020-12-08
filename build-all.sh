915b17058ce32b6f768a347c4dec7f82a4fb4e9b
YouROK <8yourok8@mail.ru>
2020-12-08 15:48:34 +0300

    sort build

--- build-all.sh
+++ build-all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 PLATFORMS=""
-PLATFORMS="$PLATFORMS darwin/amd64"              # amd64 only as of go1.5
-PLATFORMS="$PLATFORMS windows/amd64 windows/386" # arm compilation not available for Windows
 PLATFORMS="$PLATFORMS linux/amd64 linux/386"
-# PLATFORMS="$PLATFORMS linux/ppc64 linux/ppc64le"
-PLATFORMS="$PLATFORMS linux/mips linux/mipsle linux/mips64 linux/mips64le" # experimental in go1.6
-#PLATFORMS="$PLATFORMS linux/arm linux/arm64"
+PLATFORMS="$PLATFORMS windows/amd64 windows/386" # arm compilation not available for Windows
+PLATFORMS="$PLATFORMS darwin/amd64"              # amd64 only as of go1.5
 PLATFORMS="$PLATFORMS freebsd/amd64"
+PLATFORMS="$PLATFORMS linux/mips linux/mipsle linux/mips64 linux/mips64le" # experimental in go1.6
+# PLATFORMS="$PLATFORMS linux/ppc64 linux/ppc64le"
+#PLATFORMS="$PLATFORMS linux/arm linux/arm64"
 # PLATFORMS="$PLATFORMS netbsd/amd64" # amd64 only as of go1.6
 # PLATFORMS="$PLATFORMS openbsd/amd64" # amd64 only as of go1.6
 # PLATFORMS="$PLATFORMS dragonfly/amd64" # amd64 only as of go1.5
