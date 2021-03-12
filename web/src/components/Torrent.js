e03206fee01b01eea70bef67b23aba0217bed0ed
YouROK <8yourok8@mail.ru>
2021-03-12 10:13:09 +0300

    update web

--- web/src/components/Torrent.js
+++ web/src/components/Torrent.js
@@ -4,6 +4,7 @@ import Button from '@material-ui/core/Button'
 
 import 'fontsource-roboto'
 
+import CloseIcon from '@material-ui/icons/Close';
 import DeleteIcon from '@material-ui/icons/Delete'
 import Typography from '@material-ui/core/Typography'
 import ListItem from '@material-ui/core/ListItem'
@@ -74,6 +75,14 @@ export default function Torrent(props) {
                     </Button>
                     <Button
                         onClick={() => {
+                            dropTorrent(torrent)
+                        }}
+                    >
+                        <CloseIcon />
+                        <Typography>Drop</Typography>
+                    </Button>
+                    <Button
+                        onClick={() => {
                             deleteTorrent(torrent)
                         }}
                     >
@@ -101,16 +110,6 @@ export default function Torrent(props) {
                         }}
                     >
                         OK
-                    </Button>
-                    <Button
-                        variant="outlined"
-                        color="primary"
-                        onClick={() => {
-                            setOpen(false)
-                            dropTorrent(torrent)
-                        }}
-                    >
-                        Drop
                     </Button>
                 </DialogActions>
             </Dialog>
@@ -177,46 +176,3 @@ function dropTorrent(torrent) {
         console.error(e)
     }
 }
-
-/*
-{
-	"title": "Mulan 2020",
-	"poster": "https://kinohod.ru/o/88/d3/88d3054f-8fd3-4daf-8977-bb4bc8b95206.jpg",
-	"timestamp": 1606897747,
-	"name": "Mulan.2020.MVO.BDRip.1.46Gb",
-	"hash": "f6c992b437c04d0f5a44b42852bb61de7ce90f9a",
-	"stat": 2,
-	"stat_string": "Torrent preload",
-	"loaded_size": 6160384,
-	"torrent_size": 1569489783,
-	"preloaded_bytes": 5046272,
-	"preload_size": 20971520,
-	"download_speed": 737156.3390754947,
-	"total_peers": 149,
-	"pending_peers": 136,
-	"active_peers": 10,
-	"connected_seeders": 9,
-	"half_open_peers": 15,
-	"bytes_written": 100327,
-	"bytes_read": 8077590,
-	"bytes_read_data": 7831552,
-	"bytes_read_useful_data": 6160384,
-	"chunks_read": 478,
-	"chunks_read_useful": 376,
-	"chunks_read_wasted": 102,
-	"pieces_dirtied_good": 2,
-	"file_stats": [{
-		"id": 1,
-		"path": "Mulan.2020.MVO.BDRip.1.46Gb/Mulan.2020.MVO.BDRip.1.46Gb.avi",
-		"length": 1569415168
-	}, {
-		"id": 2,
-		"path": "Mulan.2020.MVO.BDRip.1.46Gb/Mulan.2020.MVO.BDRip.1.46Gb_forced.rus.srt",
-		"length": 765
-	}, {
-		"id": 3,
-		"path": "Mulan.2020.MVO.BDRip.1.46Gb/Mulan.2020.MVO.BDRip.1.46Gb_full.rus.srt",
-		"length": 73850
-	}]
-}
- */
