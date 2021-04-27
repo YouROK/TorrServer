6da6cbce6819009a60f79dc7818f9c5ddf333ee0
Arkadiy Illarionov <qarkai@gmail.com>
2021-04-27 22:41:59 +0300

    Set GOARM, GO_ARM in function

--- build-all.sh
+++ build-all.sh
@@ -11,6 +11,17 @@ PLATFORMS=(
 
 type setopt >/dev/null 2>&1
 
+set_goarm() {
+  if [[ "$1" =~ arm([5,7]) ]]; then
+    GOARCH="arm"
+    GOARM="${BASH_REMATCH[1]}"
+    GO_ARM="GOARM=${GOARM}"
+  else
+    GOARM=""
+    GO_ARM=""
+  fi
+}
+
 GOBIN="go"
 
 $GOBIN version
@@ -41,14 +52,7 @@ BUILD_FLAGS="-ldflags=${LDFLAGS}"
 for PLATFORM in "${PLATFORMS[@]}"; do
   GOOS=${PLATFORM%/*}
   GOARCH=${PLATFORM#*/}
-  if [[ "$GOARCH" =~ arm([5,7]) ]]; then
-    GOARCH="arm"
-    GOARM="${BASH_REMATCH[1]}"
-    GO_ARM="GOARM=${GOARM}"
-  else
-    GOARM=""
-    GO_ARM=""
-  fi
+  set_goarm "$GOARCH"
   BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}${GOARM}"
   if [[ "${GOOS}" == "windows" ]]; then BIN_FILENAME="${BIN_FILENAME}.exe"; fi
   CMD="GOOS=${GOOS} GOARCH=${GOARCH} ${GO_ARM} ${GOBIN} build ${BUILD_FLAGS} -o ${BIN_FILENAME} ./cmd"
@@ -74,14 +78,7 @@ GOOS=android
 for GOARCH in "${!COMPILERS[@]}"; do
   export CC="$NDK_TOOLCHAIN/bin/${COMPILERS[$GOARCH]}"
   export CXX="$NDK_TOOLCHAIN/bin/${COMPILERS[$GOARCH]}++"
-  if [ "$GOARCH" = "arm7" ]; then
-    GOARCH="arm"
-    GOARM="7"
-    GO_ARM="GOARM=${GOARM}"
-  else
-    GOARM=""
-    GO_ARM=""
-  fi
+  set_goarm "$GOARCH"
   BIN_FILENAME="${OUTPUT}-${GOOS}-${GOARCH}${GOARM}"
   CMD="GOOS=${GOOS} GOARCH=${GOARCH} ${GO_ARM} CGO_ENABLED=1 ${GOBIN} build ${BUILD_FLAGS} -o ${BIN_FILENAME} ./cmd"
   echo "${CMD}"
