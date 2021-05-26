4e93b20880c65a54a992d4922fec90e203f32b1b
Daniel Shleifman <dancheskus@gmail.com>
2021-05-26 10:18:17 +0300

    refactor

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,17 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const gulp = require('gulp')
 const inlinesource = require('gulp-inline-source')
 const replace = require('gulp-replace')
 
-gulp.task('default', () => {
-    return gulp
-        .src('./build/*.html')
-        .pipe(replace('.js"></script>', '.js" inline></script>'))
-        .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
-        .pipe(
-            inlinesource({
-                compress: false,
-                ignore: ['png'],
-            })
-        )
-        .pipe(gulp.dest('./dest'))
-})
+gulp.task('default', () =>
+  gulp
+    .src('./build/*.html')
+    .pipe(replace('.js"></script>', '.js" inline></script>'))
+    .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+    .pipe(
+      inlinesource({
+        compress: false,
+        ignore: ['png'],
+      }),
+    )
+    .pipe(gulp.dest('./dest')),
+)
