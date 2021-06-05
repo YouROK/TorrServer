ecb2403bf6e7f26dcb2b88bab02810e90a1d5bd2
nikk gitanes <tsynik@gmail.com>
2021-06-05 16:40:07 +0300

    fix fresh web build

--- build-all.sh
+++ build-all.sh
@@ -34,6 +34,7 @@ OUTPUT="${ROOT}/dist/TorrServer"
 #### Build web
 echo "Build web"
 cd "${ROOT}/web" || exit 1
+npm install --silent
 npm run --silent build-js
 cp "${ROOT}/web/dest/index.html" "${ROOT}/server/web/pages/template/pages/"
 
