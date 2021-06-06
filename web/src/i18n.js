aebcc24970f1fefdec303ca7322fe14ed8d1fe04
Daniel Shleifman <dancheskus@gmail.com>
2021-06-06 22:44:36 +0300

    refactor

--- web/src/i18n.js
+++ web/src/i18n.js
@@ -8,7 +8,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en', // default
+    fallbackLng: 'en', // default language will be used if none of declared lanuages detected (en, ru)
     interpolation: { escapeValue: false }, // react already safes from xss
     resources: { en: { translation: translationEN }, ru: { translation: translationRU } },
   })
