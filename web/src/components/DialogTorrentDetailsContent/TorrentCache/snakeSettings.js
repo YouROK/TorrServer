ce0298bcea2f53fc92ec06324b93e8e0e583714a
nikk gitanes <tsynik@gmail.com>
2021-06-16 10:40:01 +0300

    Update snakeSettings.js

--- web/src/components/DialogTorrentDetailsContent/TorrentCache/snakeSettings.js
+++ web/src/components/DialogTorrentDetailsContent/TorrentCache/snakeSettings.js
@@ -4,7 +4,7 @@ export const pieceSizeForMiniMap = 23
 export const gapBetweenPieces = 3
 export const miniCacheMaxHeight = 340
 
-export const defaultBorderColor = '#eef2f4'
+export const defaultBorderColor = '#dbf2e8'
 export const defaultBackgroundColor = '#fff'
 export const completeColor = '#00a572'
 export const progressColor = '#ffa724'
