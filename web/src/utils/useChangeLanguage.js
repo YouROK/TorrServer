4654c1d7fab5fdae0fcbe1b51236d09a8d259cb6
Daniel Shleifman <dancheskus@gmail.com>
2021-06-18 14:00:02 +0300

    language detection fix

--- web/src/utils/useChangeLanguage.js
+++ web/src/utils/useChangeLanguage.js
@@ -2,5 +2,7 @@ import { useTranslation } from 'react-i18next'
 
 export default () => {
   const { i18n } = useTranslation()
-  return [i18n.language, lang => i18n.changeLanguage(lang)]
+  const currentLanguage = i18n.language === 'en-US' || i18n.language === 'en' ? 'en' : i18n.language
+
+  return [currentLanguage, lang => i18n.changeLanguage(lang)]
 }
