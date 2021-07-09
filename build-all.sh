f7cb8afb819c14c90e91dbdd245e85933acba9cc
YouROK <8yourok8@mail.ru>
2021-07-09 12:39:44 +0300

    add gen web

--- build-all.sh
+++ build-all.sh
@@ -33,10 +33,7 @@ OUTPUT="${ROOT}/dist/TorrServer"
 
 #### Build web
 echo "Build web"
-cd "${ROOT}/web" || exit 1
-npm install --silent
-npm run --silent build-js
-cp "${ROOT}/web/dest/index.html" "${ROOT}/server/web/pages/template/pages/"
+$GOBIN run gen_web.go || exit 1
 
 #### Build server
 echo "Build server"
