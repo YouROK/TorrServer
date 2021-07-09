6d64b515f6674af84d4ab117da378c3ea6cb317c
YouROK <8yourok8@mail.ru>
2021-07-09 12:43:17 +0300

    add web

---
+++ server/web/pages/template/html.go
@@ -0,0 +1,81 @@
+package template
+
+import (
+	_ "embed"
+)
+
+
+//go:embed pages/android-chrome-192x192.png
+var Androidchrome192x192png []byte
+
+
+//go:embed pages/android-chrome-512x512.png
+var Androidchrome512x512png []byte
+
+
+//go:embed pages/apple-touch-icon.png
+var Appletouchiconpng []byte
+
+
+//go:embed pages/asset-manifest.json
+var Assetmanifestjson []byte
+
+
+//go:embed pages/browserconfig.xml
+var Browserconfigxml []byte
+
+
+//go:embed pages/favicon-16x16.png
+var Favicon16x16png []byte
+
+
+//go:embed pages/favicon-32x32.png
+var Favicon32x32png []byte
+
+
+//go:embed pages/favicon.ico
+var Faviconico []byte
+
+
+//go:embed pages/index.html
+var Indexhtml []byte
+
+
+//go:embed pages/mstile-150x150.png
+var Mstile150x150png []byte
+
+
+//go:embed pages/safari-pinned-tab.svg
+var Safaripinnedtabsvg []byte
+
+
+//go:embed pages/site.webmanifest
+var Sitewebmanifest []byte
+
+
+//go:embed pages/static/js/2.13abae58.chunk.js
+var Staticjs213abae58chunkjs []byte
+
+
+//go:embed pages/static/js/2.13abae58.chunk.js.LICENSE.txt
+var Staticjs213abae58chunkjsLICENSEtxt []byte
+
+
+//go:embed pages/static/js/2.13abae58.chunk.js.map
+var Staticjs213abae58chunkjsmap []byte
+
+
+//go:embed pages/static/js/main.b3854718.chunk.js
+var Staticjsmainb3854718chunkjs []byte
+
+
+//go:embed pages/static/js/main.b3854718.chunk.js.map
+var Staticjsmainb3854718chunkjsmap []byte
+
+
+//go:embed pages/static/js/runtime-main.8bda5920.js
+var Staticjsruntimemain8bda5920js []byte
+
+
+//go:embed pages/static/js/runtime-main.8bda5920.js.map
+var Staticjsruntimemain8bda5920jsmap []byte
