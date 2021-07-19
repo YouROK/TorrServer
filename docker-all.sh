fb1e5af2ad33499ecc22838a727a1b37e1e2e5d8
YouROK <8yourok8@mail.ru>
2021-07-19 12:40:21 +0300

    fix web build

--- docker-all.sh
+++ docker-all.sh
@@ -4,7 +4,7 @@ ROOT=${PWD}
 
 #### Build web
 echo "Build web"
-$GOBIN run gen_web.go
+go run gen_web.go
 
 sudo docker run --rm -v "$PWD":/usr/src/torr -w /usr/src/torr golang:1.16 ./build-all.sh
 sudo chmod 0777 ./dist/*
