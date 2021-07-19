a321fe3ea2977c6faab06cc349d40b5fcb10cdce
YouROK <8yourok8@mail.ru>
2021-07-19 11:34:17 +0300

    add error if file db not open

--- server/settings/settings.go
+++ server/settings/settings.go
@@ -1,5 +1,12 @@
 package settings
 
+import (
+	"os"
+	"path/filepath"
+
+	"server/log"
+)
+
 var (
 	tdb      *TDB
 	Path     string
@@ -10,6 +17,10 @@ var (
 func InitSets(readOnly bool) {
 	ReadOnly = readOnly
 	tdb = NewTDB()
+	if tdb == nil {
+		log.TLogln("Error open db:", filepath.Join(Path, "config.db"))
+		os.Exit(1)
+	}
 	loadBTSets()
 	Migrate()
 }
