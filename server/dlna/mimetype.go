afa4a3d3038135fe7c64fa86965acbd6f4edc45b
nikk gitanes <tsynik@gmail.com>
2021-09-06 03:44:03 +0300

    typos

--- server/dlna/mimetype.go
+++ server/dlna/mimetype.go
@@ -105,9 +105,9 @@ func MimeTypeByPath(filePath string) (ret mimeType, err error) {
 // TODO: make this client headers / profile map
 	if ret == "video/mp2t" {
 		ret = "video/mpeg"
-	else if ret == "video/x-matroska" {
+	} else if ret == "video/x-matroska" {
 		ret = "video/mpeg"
-	else if ret == "video/x-msvideo" {
+	} else if ret == "video/x-msvideo" {
 		ret = "video/avi"
 	} else if ret == "" {
 		ret = "application/octet-stream"
