71d7451a791bd7c7ccbfedcf405407064e6e47c4
nikk gitanes <tsynik@gmail.com>
2021-09-09 00:43:33 +0300

    fix ru-RU default display

--- web/src/utils/useChangeLanguage.js
+++ web/src/utils/useChangeLanguage.js
@@ -2,7 +2,7 @@ import { useTranslation } from 'react-i18next'
 
 export default () => {
   const { i18n } = useTranslation()
-  const currentLanguage = i18n.language === 'en-US' || i18n.language === 'en' ? 'en' : i18n.language
+  const currentLanguage = i18n.language === 'en-US' || i18n.language === 'en' ? 'en' : i18n.language === 'ru-RU' || i18n.language === 'ru' ? 'ru' : i18n.language
 
   return [currentLanguage, lang => i18n.changeLanguage(lang)]
 }
