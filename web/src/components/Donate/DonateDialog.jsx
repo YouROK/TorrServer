44ab559eb16da9d1a3ad4d6b6a94b640c1cd47b5
YouROK <8yourok8@mail.ru>
2021-09-22 14:30:20 +0300

    add qiwi

--- web/src/components/Donate/DonateDialog.jsx
+++ web/src/components/Donate/DonateDialog.jsx
@@ -9,34 +9,35 @@ import Button from '@material-ui/core/Button'
 import { useTranslation } from 'react-i18next'
 
 const donateFrame =
-  '<iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&targets=TorrServer Donate&targets-hint=&default-sum=200&button-text=14&payment-type-choice=on&mobile-payment-type-choice=on&comment=on&hint=&successURL=&quickpay=shop&account=410013733697114" width="320" height="320" frameborder="0" allowtransparency="true" scrolling="no"></iframe>'
+    '<iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&targets=TorrServer Donate&targets-hint=&default-sum=200&button-text=14&payment-type-choice=on&mobile-payment-type-choice=on&comment=on&hint=&successURL=&quickpay=shop&account=410013733697114" width="320" height="320" frameborder="0" allowtransparency="true" scrolling="no"></iframe>'
 
 export default function DonateDialog({ onClose }) {
-  const { t } = useTranslation()
+    const { t } = useTranslation()
 
-  return (
-    <Dialog open onClose={onClose} aria-labelledby='form-dialog-title' fullWidth maxWidth='xs'>
-      <DialogTitle id='form-dialog-title'>{t('Donate')}</DialogTitle>
-      <DialogContent>
-        <List>
-          <ListItem key='DonateLinks'>
-            <ButtonGroup variant='outlined' color='primary' aria-label='contained primary button group'>
-              <Button onClick={() => window.open('https://www.paypal.com/paypalme/yourok', '_blank')}>PayPal</Button>
-              <Button onClick={() => window.open('https://yoomoney.ru/to/410013733697114', '_blank')}>IO.Money</Button>
-            </ButtonGroup>
-          </ListItem>
-          <ListItem key='DonateForm'>
-            {/* eslint-disable-next-line react/no-danger */}
-            <div dangerouslySetInnerHTML={{ __html: donateFrame }} />
-          </ListItem>
-        </List>
-      </DialogContent>
+    return (
+        <Dialog open onClose={onClose} aria-labelledby='form-dialog-title' fullWidth maxWidth='xs'>
+            <DialogTitle id='form-dialog-title'>{t('Donate')}</DialogTitle>
+            <DialogContent>
+                <List>
+                    <ListItem key='DonateLinks'>
+                        <ButtonGroup variant='outlined' color='primary' aria-label='contained primary button group'>
+                            <Button onClick={() => window.open('https://www.paypal.com/paypalme/yourok', '_blank')}>PayPal</Button>
+                            <Button onClick={() => window.open('https://yoomoney.ru/to/410013733697114', '_blank')}>IO.Money</Button>
+                            <Button onClick={() => window.open('https://qiwi.com/n/YOUROK85', '_blank')}>QIWI</Button>
+                        </ButtonGroup>
+                    </ListItem>
+                    <ListItem key='DonateForm'>
+                        {/* eslint-disable-next-line react/no-danger */}
+                        <div dangerouslySetInnerHTML={{ __html: donateFrame }} />
+                    </ListItem>
+                </List>
+            </DialogContent>
 
-      <DialogActions>
-        <Button onClick={onClose} color='secondary' variant='contained'>
-          Ok
-        </Button>
-      </DialogActions>
-    </Dialog>
-  )
+            <DialogActions>
+                <Button onClick={onClose} color='secondary' variant='contained'>
+                    Ok
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
 }
