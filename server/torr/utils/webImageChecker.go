9dc219739dbd0298bc6e71fc0b84fca2aba511ce
YouROK <8YouROK8@mail.ru>
2022-03-16 18:36:59 +0300

    add image checker, web vulnerability

---
+++ server/torr/utils/webImageChecker.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"net/http"
+	"server/log"
+)
+
+func CheckImgUrl(link string) bool {
+	if link == "" {
+		return false
+	}
+	resp, err := http.Get(link)
+	if err != nil {
+		log.TLogln("Error check image:", err)
+		return false
+	}
+	defer resp.Body.Close()
+	_, _, err = image.Decode(resp.Body)
+	if err != nil {
+		log.TLogln("Error decode image:", err)
+		return false
+	}
+	return err == nil
+}
