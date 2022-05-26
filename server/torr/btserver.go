565150aedefbd613be495d283fdf3548fb7ee4e9
nikk gitanes <tsynik@gmail.com>
2022-05-26 10:59:07 +0300

    only check IPv6 if enabled

--- server/torr/btserver.go
+++ server/torr/btserver.go
@@ -77,7 +77,7 @@ func (bt *BTServer) Disconnect() {
 	}
 }
 
-func (bt *BTServer) configure(ctx context.Context) (err error) {
+func (bt *BTServer) configure(ctx context.Context) {
 	blocklist, _ := utils.ReadBlockedIP()
 	bt.config = torrent.NewDefaultClientConfig()
 
@@ -145,6 +145,8 @@ func (bt *BTServer) configure(ctx context.Context) (err error) {
 	}
 
 	log.Println("Client config:", settings.BTsets)
+	
+	var err error
 
 	// set public IPv4
 	if settings.PubIPv4 != "" {
@@ -168,7 +170,7 @@ func (bt *BTServer) configure(ctx context.Context) (err error) {
 			bt.config.PublicIp6 = ip6
 		}
 	}
-	if bt.config.PublicIp6 == nil {
+	if bt.config.PublicIp6 == nil && settings.BTsets.EnableIPv6 {
 		bt.config.PublicIp6, err = publicip.Get6(ctx)
 		if err != nil {
 			log.Printf("error getting public ipv6 address: %v", err)
@@ -177,7 +179,6 @@ func (bt *BTServer) configure(ctx context.Context) (err error) {
 	if bt.config.PublicIp6 != nil {
 		log.Println("PublicIp6:", bt.config.PublicIp6)
 	}
-	return err
 }
 
 func (bt *BTServer) GetTorrent(hash torrent.InfoHash) *Torrent {
