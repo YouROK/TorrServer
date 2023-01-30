dcc274ea802407f250dc63c6e22dd3e96eccee50
YouROK <8YouROK8@mail.ru>
2023-01-30 11:45:45 +0300

    return empty list

--- server/web/api/rutor.go
+++ server/web/api/rutor.go
@@ -3,10 +3,14 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"server/rutor"
+	"server/rutor/models"
 )
 
 func rutorSearch(c *gin.Context) {
 	query := c.Query("query")
 	list := rutor.Search(query)
+	if list == nil {
+		list = []*models.TorrentDetails{}
+	}
 	c.JSON(200, list)
 }
