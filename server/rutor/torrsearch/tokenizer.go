7e95fd6f2f2116d48b2bdf5fd25401a3f601d1c4
YouROK <8YouROK8@mail.ru>
2023-02-07 15:55:23 +0300

    remove stemmer word in search

--- server/rutor/torrsearch/tokenizer.go
+++ server/rutor/torrsearch/tokenizer.go
@@ -18,6 +18,6 @@ func analyze(text string) []string {
 	tokens := tokenize(text)
 	tokens = lowercaseFilter(tokens)
 	tokens = stopwordFilter(tokens)
-	tokens = stemmerFilter(tokens)
+	//tokens = stemmerFilter(tokens)
 	return tokens
 }
