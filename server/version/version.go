da0b11de876a4f7a80e1cdfbdcce7c26af05c471
nikk gitanes <tsynik@gmail.com>
2023-03-12 13:03:27 +0300

    fix OE torrent version

--- server/version/version.go
+++ server/version/version.go
@@ -16,8 +16,12 @@ func GetTorrentVersion() string {
 		return ""
 	}
 	for _, dep := range bi.Deps {
-		if dep.Path == "github.com/anacrolix/torrent" || dep.Path == "github.com/tsynik/torrent" {
-			return dep.Version
+		if dep.Path == "github.com/anacrolix/torrent" {
+			if dep.Replace != nil {
+				return dep.Replace.Version
+			} else {
+				return dep.Version
+			}
 		}
 	}
 	return ""
