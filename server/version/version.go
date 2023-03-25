57c0d225d32a326e6125d642413eee62941528b6
nikk gitanes <tsynik@gmail.com>
2023-03-25 08:45:52 +0300

    bump ver for tag

--- server/version/version.go
+++ server/version/version.go
@@ -6,7 +6,7 @@ import (
 	// "github.com/anacrolix/torrent"
 )
 
-const Version = "MatriX.120.9"
+const Version = "MatriX.120.10"
 
 func GetTorrentVersion() string {
 	// _ = torrent.NewDefaultClientConfig()
