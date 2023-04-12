b3837649247f911179cec4e025eda97a4d2927a1
yourok <8yourok8@mail.ru>
2023-04-12 21:30:50 +0300

    MatriX.122

--- server/version/version.go
+++ server/version/version.go
@@ -6,7 +6,7 @@ import (
 	// "github.com/anacrolix/torrent"
 )
 
-const Version = "MatriX.121.2"
+const Version = "MatriX.122"
 
 func GetTorrentVersion() string {
 	// _ = torrent.NewDefaultClientConfig()
