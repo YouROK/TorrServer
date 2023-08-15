80187393f520f63e26c0e39628540bc939e75915
yourok <8yourok8@mail.ru>
2023-08-15 12:16:35 +0300

    update web

---
+++ server/web/pages/template/html.go
@@ -0,0 +1,140 @@
+package template
+
+import (
+	_ "embed"
+)
+
+//go:embed pages/apple-splash-1125-2436.jpg
+var Applesplash11252436jpg []byte
+
+//go:embed pages/apple-splash-1136-640.jpg
+var Applesplash1136640jpg []byte
+
+//go:embed pages/apple-splash-1170-2532.jpg
+var Applesplash11702532jpg []byte
+
+//go:embed pages/apple-splash-1242-2208.jpg
+var Applesplash12422208jpg []byte
+
+//go:embed pages/apple-splash-1242-2688.jpg
+var Applesplash12422688jpg []byte
+
+//go:embed pages/apple-splash-1284-2778.jpg
+var Applesplash12842778jpg []byte
+
+//go:embed pages/apple-splash-1334-750.jpg
+var Applesplash1334750jpg []byte
+
+//go:embed pages/apple-splash-1536-2048.jpg
+var Applesplash15362048jpg []byte
+
+//go:embed pages/apple-splash-1620-2160.jpg
+var Applesplash16202160jpg []byte
+
+//go:embed pages/apple-splash-1668-2224.jpg
+var Applesplash16682224jpg []byte
+
+//go:embed pages/apple-splash-1668-2388.jpg
+var Applesplash16682388jpg []byte
+
+//go:embed pages/apple-splash-1792-828.jpg
+var Applesplash1792828jpg []byte
+
+//go:embed pages/apple-splash-2048-1536.jpg
+var Applesplash20481536jpg []byte
+
+//go:embed pages/apple-splash-2048-2732.jpg
+var Applesplash20482732jpg []byte
+
+//go:embed pages/apple-splash-2160-1620.jpg
+var Applesplash21601620jpg []byte
+
+//go:embed pages/apple-splash-2208-1242.jpg
+var Applesplash22081242jpg []byte
+
+//go:embed pages/apple-splash-2224-1668.jpg
+var Applesplash22241668jpg []byte
+
+//go:embed pages/apple-splash-2388-1668.jpg
+var Applesplash23881668jpg []byte
+
+//go:embed pages/apple-splash-2436-1125.jpg
+var Applesplash24361125jpg []byte
+
+//go:embed pages/apple-splash-2532-1170.jpg
+var Applesplash25321170jpg []byte
+
+//go:embed pages/apple-splash-2688-1242.jpg
+var Applesplash26881242jpg []byte
+
+//go:embed pages/apple-splash-2732-2048.jpg
+var Applesplash27322048jpg []byte
+
+//go:embed pages/apple-splash-2778-1284.jpg
+var Applesplash27781284jpg []byte
+
+//go:embed pages/apple-splash-640-1136.jpg
+var Applesplash6401136jpg []byte
+
+//go:embed pages/apple-splash-750-1334.jpg
+var Applesplash7501334jpg []byte
+
+//go:embed pages/apple-splash-828-1792.jpg
+var Applesplash8281792jpg []byte
+
+//go:embed pages/asset-manifest.json
+var Assetmanifestjson []byte
+
+//go:embed pages/browserconfig.xml
+var Browserconfigxml []byte
+
+//go:embed pages/dlnaicon-120.png
+var Dlnaicon120png []byte
+
+//go:embed pages/dlnaicon-48.png
+var Dlnaicon48png []byte
+
+//go:embed pages/favicon-16x16.png
+var Favicon16x16png []byte
+
+//go:embed pages/favicon-32x32.png
+var Favicon32x32png []byte
+
+//go:embed pages/favicon.ico
+var Faviconico []byte
+
+//go:embed pages/icon.png
+var Iconpng []byte
+
+//go:embed pages/index.html
+var Indexhtml []byte
+
+//go:embed pages/logo.png
+var Logopng []byte
+
+//go:embed pages/mstile-150x150.png
+var Mstile150x150png []byte
+
+//go:embed pages/site.webmanifest
+var Sitewebmanifest []byte
+
+//go:embed pages/static/js/2.a4cb6022.chunk.js
+var Staticjs2a4cb6022chunkjs []byte
+
+//go:embed pages/static/js/2.a4cb6022.chunk.js.LICENSE.txt
+var Staticjs2a4cb6022chunkjsLICENSEtxt []byte
+
+//go:embed pages/static/js/2.a4cb6022.chunk.js.map
+var Staticjs2a4cb6022chunkjsmap []byte
+
+//go:embed pages/static/js/main.489fa76c.chunk.js
+var Staticjsmain489fa76cchunkjs []byte
+
+//go:embed pages/static/js/main.489fa76c.chunk.js.map
+var Staticjsmain489fa76cchunkjsmap []byte
+
+//go:embed pages/static/js/runtime-main.64d07802.js
+var Staticjsruntimemain64d07802js []byte
+
+//go:embed pages/static/js/runtime-main.64d07802.js.map
+var Staticjsruntimemain64d07802jsmap []byte
--- web/src/components/About/index.jsx
+++ web/src/components/About/index.jsx
@@ -69,6 +69,7 @@ export default function AboutDialog() {
               <div>
                 <LinkComponent name={t('ProjectSource')} link='https://github.com/YouROK/TorrServer' />
                 <LinkComponent name={t('Releases')} link='https://github.com/YouROK/TorrServer/releases' />
+                <LinkComponent name={t('NasReleases')} link='https://github.com/vladlenas' />
               </div>
             </Section>
 
@@ -85,6 +86,7 @@ export default function AboutDialog() {
                 <LinkComponent name='damiva' link='https://github.com/damiva' />
                 <LinkComponent name='Anton Potekhin' link='https://github.com/Anton111111' />
                 <LinkComponent name='FaintGhost' link='https://github.com/FaintGhost' />
+                <LinkComponent name='vladlenas' link='https://github.com/vladlenas' />
               </div>
             </Section>
           </div>
