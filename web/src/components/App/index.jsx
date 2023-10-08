5132c65cffb8cc87b17ddca5027678b07dedc2aa
nikk gitanes <tsynik@gmail.com>
2023-10-08 23:30:04 +0300

    fix bg lang / format locales

--- web/src/components/App/index.jsx
+++ web/src/components/App/index.jsx
@@ -111,6 +111,8 @@ export default function App() {
                           ? changeLang('ua')
                           : currentLang === 'ua'
                           ? changeLang('zh')
+                          : currentLang === 'zh'
+                          ? changeLang('bg')
                           : changeLang('en')
                       }
                     >
