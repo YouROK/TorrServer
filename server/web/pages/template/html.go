3c6a9155c3275ec3bfebcce1f4185afcd41fe569
yourok <8yourok8@mail.ru>
2023-12-25 19:28:12 +0300

    update web

--- server/web/pages/template/html.go
+++ server/web/pages/template/html.go
@@ -127,11 +127,11 @@ var Staticjs2ddcd1461chunkjsLICENSEtxt []byte
 //go:embed pages/static/js/2.ddcd1461.chunk.js.map
 var Staticjs2ddcd1461chunkjsmap []byte
 
-//go:embed pages/static/js/main.4dafb19b.chunk.js
-var Staticjsmain4dafb19bchunkjs []byte
+//go:embed pages/static/js/main.c9f57604.chunk.js
+var Staticjsmainc9f57604chunkjs []byte
 
-//go:embed pages/static/js/main.4dafb19b.chunk.js.map
-var Staticjsmain4dafb19bchunkjsmap []byte
+//go:embed pages/static/js/main.c9f57604.chunk.js.map
+var Staticjsmainc9f57604chunkjsmap []byte
 
 //go:embed pages/static/js/runtime-main.f542387e.js
 var Staticjsruntimemainf542387ejs []byte
