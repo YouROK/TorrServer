efb17ee48d279e9ba711eb5fb1400127298a6c28
nikk gitanes <tsynik@gmail.com>
2024-02-03 10:40:14 +0300

    Update download.go

--- server/web/api/download.go
+++ server/web/api/download.go
@@ -47,7 +47,7 @@ func (f *fileReader) Seek(offset int64, whence int) (int64, error) {
 //
 //	@Tags			API
 //
-//	@Param			size	path	string	true	"Test file size"
+//	@Param			size	path	string	true	"Test file size (in MB)"
 //
 //	@Produce		application/octet-stream
 //	@Success		200 {file} file
