c774bbd69ae3737c7dcb1a7ffe8a349c04b10e50
Viacheslav Evseev <leporel777@gmail.com>
2024-02-22 06:01:01 +0300

    remove `not_auth` from swagger docs

--- server/docs/docs.go
+++ server/docs/docs.go
@@ -475,13 +475,6 @@ const docTemplate = `{
                         "name": "poster",
                         "in": "query",
                         "required": true
-                    },
-                    {
-                        "type": "string",
-                        "description": "Stream / playlist without authentication",
-                        "name": "not_auth",
-                        "in": "query",
-                        "required": true
                     }
                 ],
                 "responses": {
