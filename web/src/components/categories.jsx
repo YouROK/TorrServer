139874bd46c4aaeb2b11466be8d37b1603695adf
nikk gitanes <tsynik@gmail.com>
2024-04-07 05:22:30 +0300

    change movie and tv keys
    as of TMDB types

--- web/src/components/categories.jsx
+++ web/src/components/categories.jsx
@@ -4,8 +4,8 @@ import MusicNoteIcon from '@material-ui/icons/MusicNote'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 
 export const TORRENT_CATEGORIES = [
-  { key: 'movies', name: 'Movies', icon: <MovieCreationIcon /> },
-  { key: 'series', name: 'Series', icon: <LiveTvIcon /> },
+  { key: 'movie', name: 'Movies', icon: <MovieCreationIcon /> },
+  { key: 'tv', name: 'Series', icon: <LiveTvIcon /> },
   { key: 'music', name: 'Music', icon: <MusicNoteIcon /> },
   { key: 'other', name: 'Other', icon: <MoreHorizIcon /> },
 ]
