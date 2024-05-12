32fc6927ffb2671b4ab4306b40c04d8d26c69608
nikk gitanes <tsynik@gmail.com>
2024-05-12 19:17:08 +0300

    update docs and bump ver for tag
    MatriX.132.2

--- server/version/version.go
+++ server/version/version.go
@@ -6,7 +6,7 @@ import (
 	// "github.com/anacrolix/torrent"
 )
 
-const Version = "MatriX.132.1"
+const Version = "MatriX.132.2"
 
 func GetTorrentVersion() string {
 	bi, ok := debug.ReadBuildInfo()
