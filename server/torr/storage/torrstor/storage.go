e397ad159dc469545b0d4b1a6aadcb56951cac59
nikk gitanes <tsynik@gmail.com>
2024-09-14 20:43:16 +0300

    Revert "bump torrent v1.57.0 release"
    This reverts commit d6c4a5f156f11ceef0bbc878add1d4cd78370445.

--- server/torr/storage/torrstor/storage.go
+++ server/torr/storage/torrstor/storage.go
@@ -1,7 +1,6 @@
 package torrstor
 
 import (
-	"context"
 	"sync"
 
 	"server/torr/storage"
@@ -25,7 +24,7 @@ func NewStorage(capacity int64) *Storage {
 	return stor
 }
 
-func (s *Storage) OpenTorrent(_ context.Context, info *metainfo.Info, infoHash metainfo.Hash) (ts.TorrentImpl, error) {
+func (s *Storage) OpenTorrent(info *metainfo.Info, infoHash metainfo.Hash) (ts.TorrentImpl, error) {
 	capFunc := func() (int64, bool) { //	NE
 		return s.capacity, true //	NE
 	} //	NE
