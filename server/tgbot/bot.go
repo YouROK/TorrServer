11d971423417b69d3d09ddb997edb6e5ccfd32d3
YouROK <8YouROK8@mail.ru>
2025-03-22 00:14:29 +0300

    update

--- server/tgbot/bot.go
+++ server/tgbot/bot.go
@@ -1,44 +1,23 @@
 package tgbot
 
 import (
-	"encoding/json"
 	"errors"
-	"github.com/dustin/go-humanize"
 	tele "gopkg.in/telebot.v4"
 	"gopkg.in/telebot.v4/middleware"
 	"net/http"
-	"os"
-	"path/filepath"
 	"server/log"
-	"server/settings"
-	"server/torr"
-	"server/web"
+	"server/tgbot/config"
+	up "server/tgbot/upload"
 	"strconv"
 	"strings"
 	"time"
 )
 
-type Config struct {
-	WhiteIds []int64
-}
-
-var cfg *Config
-
-func init() {
-	cfg = &Config{}
-	fn := filepath.Join(settings.Path, "tg.cfg")
-	buf, err := os.ReadFile(fn)
-	if err != nil {
-		return
-	}
-	err = json.Unmarshal(buf, &cfg)
-	if err != nil {
-		log.TLogln("Error read tg config:", err)
-	}
-}
-
 func Start(token string) {
+	config.LoadConfig()
+
 	pref := tele.Settings{
+		URL:       config.Cfg.HostTG,
 		Token:     token,
 		Poller:    &tele.LongPoller{Timeout: 5 * time.Minute},
 		ParseMode: tele.ModeHTML,
@@ -53,8 +32,11 @@ func Start(token string) {
 		return
 	}
 
-	if len(cfg.WhiteIds) > 0 {
-		b.Use(middleware.Whitelist(cfg.WhiteIds...))
+	if len(config.Cfg.WhiteIds) > 0 {
+		b.Use(middleware.Whitelist(config.Cfg.WhiteIds...))
+	}
+	if len(config.Cfg.BlackIds) > 0 {
+		b.Use(middleware.Blacklist(config.Cfg.BlackIds...))
 	}
 
 	//Commands
@@ -63,14 +45,32 @@ func Start(token string) {
 	b.Handle("/help", help)
 	b.Handle("/Help", help)
 	b.Handle("/start", help)
+	b.Handle("/id", help)
 
 	b.Handle("/list", list)
+	b.Handle("/clear", clear)
 
 	//Text
 	b.Handle(tele.OnText, func(c tele.Context) error {
 		txt := c.Text()
 		if strings.HasPrefix(strings.ToLower(txt), "magnet:") || isHash(txt) {
-			return addTorrent(c, txt)
+			err := addTorrent(c, txt)
+			if err != nil {
+				return err
+			}
+			return list(c)
+		} else if c.Message().ReplyTo != nil && c.Message().ReplyTo.ReplyMarkup != nil && len(c.Message().ReplyTo.ReplyMarkup.InlineKeyboard) > 0 {
+			data := c.Message().ReplyTo.ReplyMarkup.InlineKeyboard[0][0].Data
+			if strings.HasPrefix(strings.ToLower(data), "\fall|") {
+				hash := strings.TrimPrefix(data, "\fall|")
+				from, to, err := ParseRange(c.Message().Text)
+				if err != nil {
+					c.Send("Ошибка, нужно указывать числа, пример: 2-12")
+					return err
+				}
+				up.AddRange(c, hash, from, to)
+			}
+			return nil
 		} else {
 			return c.Send("Вставьте магнет/хэш торрента чтоб добавить его на сервер")
 		}
@@ -80,75 +80,43 @@ func Start(token string) {
 		args := c.Args()
 		if len(args) > 0 {
 			if args[0] == "\ffiles" {
-				msg, err := c.Bot().Send(c.Sender(), "Подключение к торренту...")
-				t := torr.GetTorrent(args[1])
-				if t == nil {
-					c.Edit(msg, "Torrent not connected: "+args[1])
-					return nil
-				}
-				if err == nil {
-					go func() {
-						for !t.WaitInfo() {
-							time.Sleep(time.Second)
-							t = torr.GetTorrent(args[1])
-						}
-						c.Bot().Delete(msg)
-						host := settings.PubIPv4
-						if host == "" {
-							ips := web.GetLocalIps()
-							if len(ips) == 0 {
-								host = "127.0.0.1"
-							} else {
-								host = ips[0]
-							}
-						}
-
-						t = torr.GetTorrent(args[1])
-						st := t.Status()
-						txt := "Файлы:\n"
-						for _, file := range st.FileStats {
-							ff := "<b>" + filepath.Base(file.Path) + "</b> <i>" + humanize.Bytes(uint64(file.Length)) + "</i> " +
-								"<a href=\"http://" + host + ":" + settings.Port + "/stream/" + filepath.Base(file.Path) + "?link=" + t.Hash().HexString() + "&index=" + strconv.Itoa(file.Id) + "&play\">Download</a>\n\n"
-							if len(txt+ff) > 4096 {
-								c.Send(txt)
-								txt = ""
-							}
-							txt += ff
-						}
-						if len(txt) > 0 {
-							c.Send(txt)
-						}
-					}()
-				}
-				return err
+				return files(c)
 			}
 			if args[0] == "\fdelete" {
+				deleteTorrent(c)
+				return list(c)
+			}
+			if args[0] == "\fupload" {
+				return upload(c)
+			}
+			if args[0] == "\fuploadall" {
+				uploadall(c)
 				return nil
+			}
+			if args[0] == "\fcancel" {
+				if num, err := strconv.Atoi(args[1]); err == nil {
+					up.Cancel(num)
+					c.Bot().Delete(c.Callback().Message)
+					return nil
+				}
 			}
 		}
 		return errors.New("Ошибка кнопка не распознана")
 	})
 
+	up.Start()
+
 	go b.Start()
 }
 
 func help(c tele.Context) error {
+	id := strconv.FormatInt(c.Sender().ID, 10)
 	return c.Send("Бот для управления TorrServer\n\n" +
 		"Список комманд:\n" +
-		"  /help - эта справка\n" +
-		"  /list - показать список торрентов на сервере")
-}
-
-func list(c tele.Context) error {
-	list := torr.ListTorrent()
-
-	for _, t := range list {
-		btnFiles := tele.InlineButton{Text: "Файлы", Unique: "files", Data: t.Hash().String()}
-		btnDelete := tele.InlineButton{Text: "Удалить", Unique: "delete", Data: t.Hash().String()}
-		torrKbd := &tele.ReplyMarkup{InlineKeyboard: [][]tele.InlineButton{{btnFiles, btnDelete}}}
-		c.Send(t.Title+" "+humanize.Bytes(uint64(t.Size)), torrKbd)
-	}
-	return nil
+		"  /help - Эта справка\n" +
+		"  /list - Показать список торрентов на сервере\n" +
+		"  /clear - Удалить все торренты\n\n" +
+		"Ваш id: <code>" + id + "</code>, " + strings.Join([]string{c.Sender().Username, c.Sender().FirstName, c.Sender().LastName}, ", "))
 }
 
 func isHash(txt string) bool {
@@ -163,4 +131,23 @@ func isHash(txt string) bool {
 		return true
 	}
 	return false
+}
+
+func ParseRange(rng string) (int, int, error) {
+	parts := strings.Split(rng, "-")
+
+	if len(parts) != 2 {
+		return -1, -1, errors.New("Неверный формат строки")
+	}
+
+	num1, err1 := strconv.Atoi(strings.TrimSpace(parts[0]))
+	if err1 != nil {
+		return -1, -1, err1
+	}
+
+	num2, err2 := strconv.Atoi(strings.TrimSpace(parts[1]))
+	if err2 != nil {
+		return -1, -1, err2
+	}
+	return num1, num2, nil
 }
---
+++ server/tgbot/upload/manager.go
@@ -0,0 +1,244 @@
+package upload
+
+import (
+	"errors"
+	"fmt"
+	tele "gopkg.in/telebot.v4"
+	"log"
+	"math"
+	"path/filepath"
+	"server/torr"
+	"server/torr/state"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Worker struct {
+	id          int
+	c           tele.Context
+	msg         *tele.Message
+	torrentHash string
+	isCancelled bool
+	from        int
+	to          int
+	ti          *state.TorrentStatus
+}
+
+type Manager struct {
+	queue     []*Worker
+	working   map[int]*Worker
+	ids       int
+	wrkSync   sync.Mutex
+	queueLock sync.Mutex
+}
+
+func (m *Manager) Start() {
+	m.working = make(map[int]*Worker)
+	go m.work()
+}
+
+func (m *Manager) AddRange(c tele.Context, hash string, from, to int) {
+	m.queueLock.Lock()
+	defer m.queueLock.Unlock()
+
+	if len(m.queue) > 50 {
+		c.Bot().Send(c.Recipient(), "Очередь переполнена, попробуйте попозже\n\nЭлементов в очереди:"+strconv.Itoa(len(m.queue)))
+		return
+	}
+
+	m.ids++
+	if m.ids > math.MaxInt {
+		m.ids = 0
+	}
+
+	var msg *tele.Message
+	var err error
+
+	for i := 0; i < 20; i++ {
+		msg, err = c.Bot().Send(c.Recipient(), "<b>Подключение к торренту</b>\n<code>"+hash+"</code>")
+		if err == nil {
+			break
+		} else {
+			log.Println("Error send msg, try again:", i+1, "/", 20)
+		}
+	}
+
+	if err != nil {
+		log.Println("Error send msg:", err)
+		return
+	}
+
+	t := torr.GetTorrent(hash)
+	t.WaitInfo()
+	for t.Status().Stat != state.TorrentWorking {
+		time.Sleep(time.Second)
+		t = torr.GetTorrent(hash)
+	}
+	ti := t.Status()
+
+	if from == 1 && to == -1 {
+		to = len(ti.FileStats)
+	}
+	if to > len(ti.FileStats) {
+		to = len(ti.FileStats)
+	}
+	if from < 1 {
+		from = 1
+	}
+	if to >= 0 && to < from {
+		from, to = to, from
+	}
+	if to > len(ti.FileStats) {
+		to = len(ti.FileStats)
+	}
+
+	w := &Worker{
+		id:          m.ids,
+		c:           c,
+		torrentHash: hash,
+		msg:         msg,
+		ti:          ti,
+		from:        from,
+		to:          to,
+	}
+
+	m.queue = append(m.queue, w)
+}
+
+func (m *Manager) Cancel(id int) {
+	m.queueLock.Lock()
+	defer m.queueLock.Unlock()
+	var rem []int
+	for i, w := range m.queue {
+		if w.id == id {
+			w.isCancelled = true
+			w.c.Bot().Delete(w.msg)
+			rem = append(rem, i)
+			return
+		}
+	}
+	for _, i := range rem {
+		m.queue = append(m.queue[:i], m.queue[i+1:]...)
+	}
+	if wrk, ok := m.working[id]; ok {
+		wrk.isCancelled = true
+		return
+	}
+}
+
+func (m *Manager) work() {
+	for {
+		m.queueLock.Lock()
+		if len(m.working) > 0 {
+			m.queueLock.Unlock()
+			m.sendQueueStatus()
+			time.Sleep(time.Second)
+			continue
+		}
+		if len(m.queue) == 0 {
+			m.queueLock.Unlock()
+			time.Sleep(time.Second)
+			continue
+		}
+		wrk := m.queue[0]
+		m.queue = m.queue[1:]
+		m.working[wrk.id] = wrk
+		m.queueLock.Unlock()
+
+		m.sendQueueStatus()
+
+		loading(wrk)
+
+		m.queueLock.Lock()
+		delete(m.working, wrk.id)
+		m.queueLock.Unlock()
+	}
+}
+
+func (m *Manager) sendQueueStatus() {
+	m.queueLock.Lock()
+	defer m.queueLock.Unlock()
+	for i, wrk := range m.queue {
+		if wrk.msg == nil {
+			continue
+		}
+		torrKbd := &tele.ReplyMarkup{}
+		torrKbd.Inline([]tele.Row{torrKbd.Row(torrKbd.Data("Отмена", "cancel", strconv.Itoa(wrk.id)))}...)
+
+		msg := "Номер в очереди " + strconv.Itoa(i+1)
+
+		wrk.c.Bot().Edit(wrk.msg, msg, torrKbd)
+	}
+}
+
+func loading(wrk *Worker) {
+	iserr := false
+
+	t := torr.GetTorrent(wrk.torrentHash)
+	t.WaitInfo()
+	for t.Status().Stat != state.TorrentWorking {
+		time.Sleep(time.Second)
+		t = torr.GetTorrent(wrk.torrentHash)
+	}
+	wrk.ti = t.Status()
+
+	for i := wrk.from - 1; i <= wrk.to-1; i++ {
+		file := wrk.ti.FileStats[i]
+		if wrk.isCancelled {
+			return
+		}
+
+		err := uploadFile(wrk, file, i+1, len(wrk.ti.FileStats))
+		if err != nil {
+			errstr := fmt.Sprintf("Ошибка загрузки в телеграм: %v", err.Error())
+			wrk.c.Bot().Edit(wrk.msg, errstr)
+			iserr = true
+			break
+		}
+	}
+	if !iserr {
+		wrk.c.Bot().Delete(wrk.msg)
+	}
+}
+
+func uploadFile(wrk *Worker, file *state.TorrentFileStat, fi, fc int) error {
+	caption := filepath.Base(file.Path)
+	torrFile, err := NewTorrFile(wrk, file)
+	if err != nil {
+		return err
+	}
+
+	var wa sync.WaitGroup
+	wa.Add(1)
+	complete := false
+	go func() {
+		for !complete {
+			updateLoadStatus(wrk, torrFile, fi, fc)
+			time.Sleep(1 * time.Second)
+		}
+		wa.Done()
+	}()
+
+	d := &tele.Document{}
+	d.FileName = file.Path
+	d.Caption = caption
+	d.File.FileReader = torrFile
+
+	for i := 0; i < 20; i++ {
+		err = wrk.c.Send(d)
+		if err == nil || errors.Is(err, ERR_STOPPED) {
+			break
+		} else {
+			log.Println("Error send msg, try again:", i+1, "/", 20)
+		}
+	}
+
+	complete = true
+	wa.Wait()
+	torrFile.Close()
+	if errors.Is(err, ERR_STOPPED) {
+		err = nil
+	}
+	return err
+}
