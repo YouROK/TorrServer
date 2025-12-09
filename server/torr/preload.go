44077202e34d04fd85dd38fdf1699497735c53a0
nikk gitanes <tsynik@gmail.com>
2025-12-09 14:19:55 +0300

    failsafe preload reader checks

--- server/torr/preload.go
+++ server/torr/preload.go
@@ -95,72 +95,75 @@ func (t *Torrent) Preload(index int, size int64) {
 			startend = 8 << 20
 		}
 
-		readerStart := file.NewReader()
-		defer readerStart.Close()
-		readerStart.SetResponsive()
-		readerStart.SetReadahead(0)
-		readerStartEnd := size - startend
+		if t.Stat == state.TorrentPreload {
+			readerStart := file.NewReader()
+			defer readerStart.Close()
+			readerStart.SetResponsive()
+			readerStart.SetReadahead(0)
+			readerStartEnd := size - startend
 
-		if readerStartEnd < 0 {
-			// Если конец начального ридера оказался за началом
-			readerStartEnd = size
-		}
-		if readerStartEnd > file.Length() {
-			// Если конец начального ридера оказался после конца файла
-			readerStartEnd = file.Length()
-		}
+			if readerStartEnd < 0 {
+				// Если конец начального ридера оказался за началом
+				readerStartEnd = size
+			}
+			if readerStartEnd > file.Length() {
+				// Если конец начального ридера оказался после конца файла
+				readerStartEnd = file.Length()
+			}
 
-		readerEndStart := file.Length() - startend
-		readerEndEnd := file.Length()
+			readerEndStart := file.Length() - startend
+			readerEndEnd := file.Length()
 
-		var wg sync.WaitGroup
-		go func() {
-			offset := int64(0)
-			if readerEndStart > readerStartEnd {
-				// Если конечный ридер не входит в диапозон начального
-				wg.Add(1)
-				defer wg.Done()
-				if t.Stat == state.TorrentPreload {
-					readerEnd := file.NewReader()
-					readerEnd.SetResponsive()
-					readerEnd.SetReadahead(0)
-					readerEnd.Seek(readerEndStart, io.SeekStart)
-					offset = readerEndStart
-					tmp := make([]byte, 32768)
-					for offset+int64(len(tmp)) < readerEndEnd {
-						n, err := readerEnd.Read(tmp)
-						if err != nil {
-							break
+			var wg sync.WaitGroup
+			go func() {
+				offset := int64(0)
+				if readerEndStart > readerStartEnd {
+					// Если конечный ридер не входит в диапозон начального
+					wg.Add(1)
+					defer wg.Done()
+					if t.Stat == state.TorrentPreload {
+						readerEnd := file.NewReader()
+						readerEnd.SetResponsive()
+						readerEnd.SetReadahead(0)
+						readerEnd.Seek(readerEndStart, io.SeekStart)
+						offset = readerEndStart
+						tmp := make([]byte, 32768)
+						for offset+int64(len(tmp)) < readerEndEnd {
+							n, err := readerEnd.Read(tmp)
+							if err != nil {
+								break
+							}
+							offset += int64(n)
 						}
-						offset += int64(n)
+						readerEnd.Close()
 					}
-					readerEnd.Close()
 				}
-			}
-		}()
+			}()
 
-		pieceLength := t.Info().PieceLength
-		readahead := pieceLength * 4
-		if readerStartEnd < readahead {
-			readahead = 0
-		}
-		readerStart.SetReadahead(readahead)
-		offset := int64(0)
-		tmp := make([]byte, 32768)
-		for offset+int64(len(tmp)) < readerStartEnd {
-			n, err := readerStart.Read(tmp)
-			if err != nil {
-				log.TLogln("Error preload:", err)
-				return
-			}
-			offset += int64(n)
-			if readahead > 0 && readerStartEnd-(offset+int64(len(tmp))) < readahead {
+			pieceLength := t.Info().PieceLength
+			readahead := pieceLength * 4
+			if readerStartEnd < readahead {
 				readahead = 0
-				readerStart.SetReadahead(0)
 			}
-		}
+			readerStart.SetReadahead(readahead)
+			offset := int64(0)
+			tmp := make([]byte, 32768)
+			for offset+int64(len(tmp)) < readerStartEnd {
+				n, err := readerStart.Read(tmp)
+				if err != nil {
+					log.TLogln("Error preload:", err)
+					return
+				}
+				offset += int64(n)
+				if readahead > 0 && readerStartEnd-(offset+int64(len(tmp))) < readahead {
+					readahead = 0
+					readerStart.SetReadahead(0)
+				}
 
-		wg.Wait()
+			}
+
+			wg.Wait()
+		}
 	}
 	log.TLogln("End preload:", file.Torrent().InfoHash().HexString(), "Peers:", t.Torrent.Stats().ActivePeers, "/", t.Torrent.Stats().TotalPeers, "[ Seeds:", t.Torrent.Stats().ConnectedSeeders, "]")
 }
