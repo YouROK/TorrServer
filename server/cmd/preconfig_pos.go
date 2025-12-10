8b0ff49832afa12648ef889fde3bcf9121c595d2
YouROK <8yourok8@mail.ru>
2025-12-10 22:46:04 +0300

    stop server on signal

--- server/cmd/preconfig_pos.go
+++ server/cmd/preconfig_pos.go
@@ -6,6 +6,7 @@ package main
 import (
 	"os"
 	"os/signal"
+	"server"
 	"syscall"
 
 	"server/log"
@@ -13,23 +14,23 @@ import (
 )
 
 func Preconfig(dkill bool) {
-	if dkill {
-		sigc := make(chan os.Signal, 1)
-		signal.Notify(sigc,
-			syscall.SIGHUP,
-			syscall.SIGINT,
-			syscall.SIGPIPE,
-			syscall.SIGTERM,
-			syscall.SIGQUIT)
-		go func() {
-			for s := range sigc {
-				if dkill {
-					if settings.BTsets.EnableDebug || s != syscall.SIGPIPE {
-						log.TLogln("Signal catched:", s)
-						log.TLogln("To stop server, close it from web / api")
-					}
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGPIPE,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		for s := range sigc {
+			if dkill {
+				if settings.BTsets.EnableDebug || s != syscall.SIGPIPE {
+					log.TLogln("Signal catched:", s)
+					log.TLogln("To stop server, close it from web / api")
 				}
+			} else {
+				server.Stop()
 			}
-		}()
-	}
+		}
+	}()
 }
