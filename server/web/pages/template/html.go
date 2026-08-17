b8112097c512caf47267985ce5a52f11b8385c27
nikk gitanes <tsynik@gmail.com>
2026-08-17 17:26:17 +0300

    update static web

--- server/web/pages/template/html.go
+++ server/web/pages/template/html.go
@@ -130,11 +130,11 @@ var Staticjs2f454f580chunkjsLICENSEtxt []byte
 //go:embed pages/static/js/2.f454f580.chunk.js.map
 var Staticjs2f454f580chunkjsmap []byte
 
-//go:embed pages/static/js/main.b8853c56.chunk.js
-var Staticjsmainb8853c56chunkjs []byte
+//go:embed pages/static/js/main.66102304.chunk.js
+var Staticjsmain66102304chunkjs []byte
 
-//go:embed pages/static/js/main.b8853c56.chunk.js.map
-var Staticjsmainb8853c56chunkjsmap []byte
+//go:embed pages/static/js/main.66102304.chunk.js.map
+var Staticjsmain66102304chunkjsmap []byte
 
 //go:embed pages/static/js/runtime-main.5ed86a79.js
 var Staticjsruntimemain5ed86a79js []byte
--- server/web/pages/template/route.go
+++ server/web/pages/template/route.go
@@ -308,18 +308,18 @@ func RouteWebPages(route gin.IRouter) {
 		c.Data(200, "application/json", Staticjs2f454f580chunkjsmap)
 	})
 
-	route.GET("/static/js/main.b8853c56.chunk.js", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Staticjsmainb8853c56chunkjs))
+	route.GET("/static/js/main.66102304.chunk.js", func(c *gin.Context) {
+		etag := fmt.Sprintf("%x", md5.Sum(Staticjsmain66102304chunkjs))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "application/javascript; charset=utf-8", Staticjsmainb8853c56chunkjs)
+		c.Data(200, "application/javascript; charset=utf-8", Staticjsmain66102304chunkjs)
 	})
 
-	route.GET("/static/js/main.b8853c56.chunk.js.map", func(c *gin.Context) {
-		etag := fmt.Sprintf("%x", md5.Sum(Staticjsmainb8853c56chunkjsmap))
+	route.GET("/static/js/main.66102304.chunk.js.map", func(c *gin.Context) {
+		etag := fmt.Sprintf("%x", md5.Sum(Staticjsmain66102304chunkjsmap))
 		c.Header("Cache-Control", "public, max-age=31536000")
 		c.Header("ETag", etag)
-		c.Data(200, "application/json", Staticjsmainb8853c56chunkjsmap)
+		c.Data(200, "application/json", Staticjsmain66102304chunkjsmap)
 	})
 
 	route.GET("/static/js/runtime-main.5ed86a79.js", func(c *gin.Context) {
