91713659948b2a46ffd3a665234c4c813342e4fd
nikk gitanes <tsynik@gmail.com>
2026-08-28 16:54:14 +0300

    MatriX.144

--- server/version/version.go
+++ server/version/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version is set at build time via -ldflags "-X server/version.Version=<tag>"
-var Version = "MatriX.143"
+var Version = "MatriX.144"
 
 func GetTorrentVersion() string {
 	bi, ok := debug.ReadBuildInfo()
